a0aef5a7675784bcf3eca592a3088a7dc5e7b3e4
Pascal Gauthier <pgauthier@nihilisme.ca>
2025-02-10 21:45:49 -0500

    Add test
    Signed-off-by: Pascal Gauthier <pgauthier@nihilisme.ca>

--- internal/pkg/pvcscaler/dataset_test.go
+++ internal/pkg/pvcscaler/dataset_test.go
@@ -1,10 +1,13 @@
 package pvcscaler
 
 import (
+	"os"
+	"path/filepath"
 	"testing"
 
-	"github.com/stretchr/testify/assert"
 	"laghoule/pvcscaler/internal/pkg/k8s"
+
+	"github.com/stretchr/testify/assert"
 )
 
 func createDataset() dataset {
@@ -20,10 +23,25 @@ func createDataset() dataset {
 	}
 }
 
-func TestReadWorkloadsFromFile(t *testing.T) {
+func TestReadFromFile(t *testing.T) {
 	expected := createDataset()
 	dataset := dataset{}
 	err := dataset.ReadFromFile("testdata/pvcscaler.json")
 	assert.NoError(t, err)
 	assert.Equal(t, expected, dataset)
+}
+
+func TestWriteToFile(t *testing.T) {
+	dataset := dataset{}
+	tmpDir := t.TempDir()
+
+	err := dataset.WritetToFile(filepath.Join(tmpDir, "pvcscaler.json"))
+	assert.NoError(t, err)
+
+	expected, err := os.ReadFile("testdata/pvcscaler.json")
+	assert.NoError(t, err)
+	
+	actual, err := os.ReadFile(filepath.Join(tmpDir, "pvcscaler.json"))
+	assert.NoError(t, err)
+	assert.Equal(t, string(expected), string(actual))
 }
