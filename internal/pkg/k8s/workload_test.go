3edf1e71162576908d2c9e3f00c81c66cb6a4b78
Pascal Gauthier <pgauthier@nihilisme.ca>
2025-03-04 09:03:07 -0500

    Refactor TestGetDeploymentWorkloads
    Signed-off-by: Pascal Gauthier <pgauthier@nihilisme.ca>

--- internal/pkg/k8s/workload_test.go
+++ internal/pkg/k8s/workload_test.go
@@ -5,6 +5,8 @@ import (
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+	appsv1 "k8s.io/api/apps/v1"
+	corev1 "k8s.io/api/core/v1"
 	"k8s.io/client-go/kubernetes"
 )
 
@@ -53,15 +55,58 @@ func TestGetDeploymentWorkloads(t *testing.T) {
 		t.Error(err)
 	}
 
-	createDeployment(c.ClientSet)
 	createPVC(c.ClientSet, "nginx-pvc", namespace, "standard")
 
-	wloads, err := c.GetDeploymentWorkloads(context.TODO(), namespace, "standard")
-	assert.NoError(t, err)
+	tests := []struct {
+		name         string
+		dep          *appsv1.Deployment
+		storageClass string
+		expected     []Workload
+		error        bool
+	}{
+		{
+			name:         "deployment with pvc",
+			dep:          createDeployment(c.ClientSet),
+			storageClass: "standard",
+			expected: []Workload{
+				{
+					Name:      "nginx-deployment",
+					Namespace: namespace,
+					Kind:      "Deployment",
+					Replicas:  1,
+				},
+			},
+			error: false,
+		},
+		{
+			name:         "deployment with pvc and wrong storage class",
+			dep:          createDeployment(c.ClientSet),
+			storageClass: "not found",
+			expected:     []Workload{},
+			error:        false,
+		},
+		{
+			name: "deployment with no pvc",
+			dep: &appsv1.Deployment{
+				Spec: appsv1.DeploymentSpec{
+					Template: corev1.PodTemplateSpec{},
+				},
+			},
+			expected: []Workload{},
+			error:    false,
+		},
+	}
 
-	assert.Equal(t, 1, len(wloads))
-	assert.Equal(t, "nginx-deployment", wloads[0].Name)
-	assert.Equal(t, namespace, wloads[0].Namespace)
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			actual, err := c.GetDeploymentWorkloads(context.TODO(), tt.dep.Namespace, tt.storageClass)
+			if tt.error {
+				assert.Error(t, err)
+			} else {
+				assert.Equal(t, tt.expected, actual)
+			}
+		})
+	}
 }
 
 func TestGetStatefulSetWorkloads(t *testing.T) {
