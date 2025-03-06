dd1b2333ca72a4900090e322cb7c8a4abbf93965
Pascal Gauthier <pgauthier@nihilisme.ca>
2025-03-05 20:09:19 -0500

    Refactor TestGetAllNamespaces()
    Signed-off-by: Pascal Gauthier <pgauthier@nihilisme.ca>

--- internal/pkg/k8s/k8s_test.go
+++ internal/pkg/k8s/k8s_test.go
@@ -54,24 +54,50 @@ func TestNewClient(t *testing.T) {
 	}
 }
 
-func createTestNamespace() *corev1.Namespace {
-	return &corev1.Namespace{
+func createNamespace(c kubernetes.Interface) *corev1.Namespace  {
+	nsObj := &corev1.Namespace{
 		ObjectMeta: metav1.ObjectMeta{
 			Name: "default",
 		},
 	}
+	ns, _ := c.CoreV1().Namespaces().Create(context.Background(), nsObj, metav1.CreateOptions{})
+	return ns
 }
 
 func TestGetAllNamespaces(t *testing.T) {
-	client, err := NewTestClient()
-	assert.NoError(t, err)
-	assert.NotNil(t, client)
-
-	ns := createTestNamespace()
-	_, err = client.ClientSet.CoreV1().Namespaces().Create(context.TODO(), ns, metav1.CreateOptions{})
+	c, err := NewTestClient()
 	assert.NoError(t, err)
 
-	namespaces, err := client.GetAllNamespaces(context.TODO())
+	createNamespace(c.ClientSet)
+
+	tests := []struct {
+		name    string
+		expected corev1.Namespace
+		error bool
+	}{
+		{
+			name: "get all namespaces",
+			expected: corev1.Namespace{
+				ObjectMeta: metav1.ObjectMeta{
+					Name: "default",
+				},
+			},
+			error: false,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			actual, err := c.GetAllNamespaces(context.TODO())
+			if tt.error {
+				assert.Error(t, err)
+			} else {
+				assert.Equal(t, tt.expected.Name, actual[0])
+			}
+		})
+	}
+
+	namespaces, err := c.GetAllNamespaces(context.TODO())
 	assert.NoError(t, err)
 	assert.NotNil(t, namespaces)
 	assert.Equal(t, "default", namespaces[0])
